ea1fd1d75a3d80c9d185b58ab2410dbdb0ed7171
Denis Siavichay <dbsiavichay@gmail.com>
2015-03-30 21:12:14 -0500

    Controlador de Asociaciones y despliegue de datos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 (function(){
 	var app = angular.module('botiquin', []);
 
-	app.controller('Controlador',['$scope', function($scope){
-		$scope.resultado = 3+1;
+	app.controller('AsociacionController',['$scope', '$http',function($scope, $http){
+		$http.get('http://localhost:8000/asociaciones/?tecnico=2')
+			.success(function(data){
+				$scope.asociaciones = data;
+				$scope.asociaciones.every(function(element, index, array){
+					element.tecnico = JSON.parse(element.tecnico);
+					element.ubicacion = JSON.parse(element.ubicacion);
+				})				
+			});
 	}]);
 })();
